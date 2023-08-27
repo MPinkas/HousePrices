c4328a63d1bd471ff444ed353af6948e2b23bf5b
MPinkas <matan.pinkas@gmail.com>
2023-08-27 23:57:31 +0300

    in clean_lot_data - split up the indexes of all features according to how they need to be adjusted
    verifyied that all 1-hot rows manage to pass through the 1-hot function
    
    wrote function grade-index which translate rows with verbal grades (stuff like excellect-good-poor scale) into numerical grades

--- main.py
+++ main.py
@@ -10,21 +10,16 @@ import tensorflow as tf
 def load_data():
     dir = os.getcwd()
     path = "\\data\\train.csv"
+    path2 = "\\data\\data.txt"
 
     df = pd.read_csv(dir + path)
+
     X = df.to_numpy()
     X = X[:, 1:-1]  # remove labels from training data and index of row (which is just position + 1)
     y = X[:, -1]  # labels for training data
 
-    one_hot_index(X, 5)
     clean_lot_data(X)
 
-    # features 3, 4
-    f34 = X[:, 2:4]
-
-    zzz = X[0]
-    print(X[0])
-    print(X[1])
     return X
 
 
@@ -50,28 +45,55 @@ def one_hot_index(X: np.array, index: int, nan_data=False):
 
     return one_hotted, items
 
+def grade_index(X: np.array, index: int, grading: np.array, nan_data=False):
+
+    data = X[:, index]
+    graded = np.zeros(shape=data.shape)
+
+    for i in range(grading.shape[0]):
+        search = np.where(data == grading[i])[0]
+        graded[search] = i
+
+    return graded, grading
+
+
 
 def clean_lot_data(X: np.array):
 
-    type_of_dwelling, tod_index = one_hot_index(X, 0)
+    zzz = X[:, 0]
+    to_one_hot = [0, 1, 4, 7, 9, 11, 12, 13, 14, 15, 20, 21, 22, 23, 28, 38, 64, 77, 78]
+    to_one_hot_nan = [5, 24, 41, 57, 59, 73]
 
-    zoning, zoning_index = one_hot_index(X, 1)
+    no_adjustnebts = [2, 3, 8, 10, 16, 17, 18, 19, 25, 33, 35, 36, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 53, 55, 60,
+                      61, 65, 66, 67, 68, 69, 70, 74, 75]
 
-    access, access_index = one_hot_index(X, 4)
+    graded_index = [6, 10, 26, 27, 39, 40, 52, 54]
+    graded_index_nan = [29, 30, 31, 32, 34, 56, 62, 63, 71, 72]
 
-    alley_access, alley_access_index = one_hot_index(X, 5, nan_data=True)
+    to_adjust = [8, 18, 19, 58, 76]
 
-    # feature 7
-    lot_shape = X[:, 6]
+    not_needed = [37]
 
-    new_lot_shape = np.zeros(shape=lot_shape.shape)
-    shapes = ["Reg", "IR1", "IR2", "IR3"]
+    # CONSIDER - adjust features like exeterion1st and 2nd (that go together) so that they can be one hotted together
+    # ADD - function that receives data with textual scale and convert it to number grades
+    # check how no garage (index 57) interacts with garage year built (58), verify garage size is 0 (59, 60)
 
-    for i in range(len(shapes)):
-        search = np.where(lot_shape == shapes[i])[0]
-        new_lot_shape[search] = i
+    zzz = X[:, 7]
 
-    lot_shape = new_lot_shape
+    '''
+    for i in range(78):
+        try:
+            zzz = np.unique(X[:, i])
+        except TypeError:
+            print(i)
+    '''
+
+    a, b = grade_index(X, 6, np.array(['Reg', 'IR1', 'IR2', 'IR3']))
+
+    yyy = 0
+
+
+
 
 
 # Press the green button in the gutter to run the script.
